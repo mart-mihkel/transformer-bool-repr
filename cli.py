98b90b36492e59278b0008c37dd595eb1490d530
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-12-07 23:04:01 +0200

    train loop

--- cli.py
+++ cli.py
@@ -10,25 +10,56 @@ app = typer.Typer()
 
 
 @app.command()
-def data(
+def train(
     function_class: Literal[
         "conjunction", "disjunction", "parity", "majority"
     ] = "conjunction",
     input_dim: int = 8,
     seq_length: int = 32,
+    epochs: int = 25,
+    batch_size: int = 32,
+    out_dir: str = "out/train",
     random_seed: int | None = None,
 ):
-    from boolrepr.data import BooleanFunctionDataset, data_visualizer
+    from pathlib import Path
 
-    dataset = BooleanFunctionDataset(
-        num_samples=2,
+    from boolrepr.data import BooleanFunctionDataset
+    from boolrepr.models import FeedForwardNetwork
+    from boolrepr.trainer import Trainer
+
+    train_data = BooleanFunctionDataset(
+        num_samples=10_000,
         seq_length=seq_length,
         input_dim=input_dim,
         function_class=function_class,
         seed=random_seed,
     )
 
-    data_visualizer(function_class, input_dim, seq_length, dataset)
+    eval_data = BooleanFunctionDataset(
+        num_samples=1000,
+        seq_length=seq_length,
+        input_dim=input_dim,
+        function_class=function_class,
+        seed=random_seed,
+    )
+
+    ffn = FeedForwardNetwork(
+        input_size=input_dim * seq_length,
+        hidden_size=256,
+        out_size=seq_length,
+    )
+
+    trainer = Trainer(
+        model=ffn,
+        train_data=train_data,
+        eval_data=eval_data,
+        epochs=epochs,
+        batch_size=batch_size,
+        out_dir=Path(out_dir),
+        collate_fn=BooleanFunctionDataset.collate_fn_feed_forward,
+    )
+
+    trainer.train()
 
 
 if __name__ == "__main__":
