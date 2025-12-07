c4cc784e368f8d7b8df53f52100da0a6c78c6e2f
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-12-07 13:44:40 +0200

    test transformer encoder

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,11 @@
 import torch
 
-from boolrepr.models import ParallelFeedForwardNetworks
+from boolrepr.models import (
+    ParallelFeedForwardNetworks,
+    MultiHeadAttention,
+    TransformerEncoder,
+    TransformerEncoderBlock,
+)
 
 
 def test_parallel_networks():
@@ -22,8 +27,44 @@ def test_parallel_networks():
     assert y.shape == (num_models, batch_size, out_size)
 
 
-# def test_self_attention():
-#     b, n, h, e = 32, 64, 12, 48
-#     q, k, v = torch.randn(3, b, n, h, e // h)
-#     attn = TransformerBlock.self_attention(q, k, v)
-#     assert attn.shape == (b, n, h, e // h)
+def test_multi_head_attention():
+    batch, emb, seq, heads = 4, 8, 6, 4
+
+    attn = MultiHeadAttention(d_embed=emb, n_heads=heads)
+    x = torch.randn(batch, seq, emb)
+    y = attn.forward(x)
+
+    assert y.shape == (batch, seq, emb)
+
+
+def test_transformer_encoder_block():
+    batch, emb, seq, heads = 4, 8, 6, 4
+
+    enc_block = TransformerEncoderBlock(
+        embed_dim=emb,
+        num_heads=heads,
+        hidden_dim=seq,
+    )
+
+    x = torch.randn(batch, seq, emb)
+    y = enc_block.forward(x)
+
+    assert y.shape == (batch, seq, emb)
+
+
+def test_transformer_encoder():
+    voc, batch, emb, seq, heads, blocks, classes = 10, 4, 8, 6, 4, 12, 14
+
+    enc = TransformerEncoder(
+        voc_size=voc,
+        embed_dim=emb,
+        num_heads=heads,
+        hidden_dim=seq,
+        num_blocks=blocks,
+        num_classes=classes,
+    )
+
+    x = torch.randint(low=1, high=voc, size=(batch, seq))
+    y = enc.forward(x)
+
+    assert y.shape == (batch, classes)
