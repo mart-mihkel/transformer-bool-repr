fd95be003afd3db9dd097bfed65228f725916fd9
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-12-07 12:22:16 +0200

    ffn tensor type annotations

--- boolrepr/models.py
+++ boolrepr/models.py
@@ -1,30 +1,41 @@
+"""
+Model architectures from incontext bool paper: https://arxiv.org/abs/2310.03016
+"""
+
+import logging
+from typing import Annotated
+
 import torch
+from torch import Tensor
+from torch.nn import Linear, Module, ModuleList, ReLU, Sequential
 
-from torch import nn, Tensor
+logger = logging.getLogger("boolrepr")
 
 
-class NeuralNetwork(nn.Module):
+class FeedForwardNetwork(Module):
     def __init__(
         self,
         in_size: int = 64,
         hidden_size: int = 256,
         out_size: int = 1,
     ):
-        super(NeuralNetwork, self).__init__()
+        super(FeedForwardNetwork, self).__init__()
 
-        self.net = nn.Sequential(
-            nn.Linear(in_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, out_size),
+        self.net = Sequential(
+            Linear(in_size, hidden_size),
+            ReLU(),
+            Linear(hidden_size, hidden_size),
+            ReLU(),
+            Linear(hidden_size, out_size),
         )
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(
+        self, x: Annotated[Tensor, "batch input"]
+    ) -> Annotated[Tensor, "batch out"]:
         return self.net(x)
 
 
-class ParallelNetworks(nn.Module):
+class ParallelFeedForwardNetworks(Module):
     def __init__(
         self,
         num_models: int,
@@ -34,11 +45,11 @@ class ParallelNetworks(nn.Module):
     ):
         assert num_models > 0, f"Invalid number of networks: {num_models}"
 
-        super(ParallelNetworks, self).__init__()
+        super(ParallelFeedForwardNetworks, self).__init__()
 
-        self.nets = nn.ModuleList(
+        self.nets = ModuleList(
             [
-                NeuralNetwork(
+                FeedForwardNetwork(
                     in_size=in_size,
                     hidden_size=hidden_size,
                     out_size=out_size,
@@ -47,7 +58,9 @@ class ParallelNetworks(nn.Module):
             ]
         )
 
-    def forward(self, xs: Tensor) -> Tensor:
+    def forward(
+        self, xs: Annotated[Tensor, "batch networks hidden"]
+    ) -> Annotated[Tensor, "batch networks out"]:
         assert len(self.nets) == xs.shape[0], "Invalid input shape"
 
         out = [net(x) for net, x in zip(self.nets, xs)]
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,18 +1,18 @@
 import torch
 
-from boolrepr.models import ParallelNetworks
+from boolrepr.models import ParallelFeedForwardNetworks
 
 
 def test_parallel_networks():
-    num_models = 5
-    batch_size = 32
-    in_size = 64
+    num_models = 2
+    batch_size = 4
+    in_size = 6
     out_size = 1
 
-    model = ParallelNetworks(
+    model = ParallelFeedForwardNetworks(
         num_models=num_models,
         in_size=in_size,
-        hidden_size=256,
+        hidden_size=2,
         out_size=out_size,
     )
 
@@ -20,3 +20,10 @@ def test_parallel_networks():
     y = model.forward(x)
 
     assert y.shape == (num_models, batch_size, out_size)
+
+
+# def test_self_attention():
+#     b, n, h, e = 32, 64, 12, 48
+#     q, k, v = torch.randn(3, b, n, h, e // h)
+#     attn = TransformerBlock.self_attention(q, k, v)
+#     assert attn.shape == (b, n, h, e // h)
