f27889b7025489b34858ead5bbe57514a7abe608
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2025-12-09 13:14:16 +0200

    Testing > BooleanFunctionDataset - Updated test correctness from structure changes

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,20 +2,19 @@ from boolrepr.data import BooleanFunctionDataset
 
 
 def test_data_shape():
-    n_samp, seq, d_in = 1, 4, 2
+    d_in = 2
+    seq = 2**d_in
+
     dataset = BooleanFunctionDataset(
         function_class="conjunction",
-        num_samples=n_samp,
-        seq_length=seq,
         input_dim=d_in,
-        noise_prob=0,
         random_seed=1,
     )
 
     data = dataset.data
-    x = data[0]["x"]
-    y = data[0]["y"]
+    x = data["x"]
+    y = data["y"]
 
-    assert len(data) == n_samp
+    assert len(data) == 2
     assert x.shape == (seq, d_in)
     assert y.shape == (seq,)
